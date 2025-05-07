3a79bbcb327d4ee93753cb91abdbaa324c2f086b
Dennis Plank <dplank@sayari.com>
2025-05-06 20:37:58 -0600

    added tsconfig dependencies

--- tests/api/interactions.test.js
+++ tests/api/interactions.test.js
@@ -2,10 +2,12 @@ require('dotenv').config({ path: '.env.test' });
 
 const request = require('supertest');
 const app = require('../../backend/src/api/server.ts'); 
+console.log(process.env.DATABASE_URL);
 
 
 describe('GET /api/interactions', () => {
     it('should return all interactions', async () => {
-      console.log(app ? 'App is defined' : 'App is not defined');
+        // const response = await request(app).get('/api/interactions');
+        console.log("worked");
   });
 }); 
